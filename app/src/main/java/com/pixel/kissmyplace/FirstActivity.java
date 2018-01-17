143c654e964ba43eb180dfbb515e7038c3364671
47hm4n3 <pixel@client-eduroam718.canalip.upmc.fr>
2018-01-17 14:39:50 +0100

    First commit

---
+++ app/src/main/java/com/pixel/kissmyplace/FirstActivity.java
@@ -0,0 +1,22 @@
+package com.pixel.kissmyplace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Created by pixel on 17/01/2018.
+ */
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+    }
+
+}
