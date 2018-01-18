4d38311137269575180b59f663d3f781d4f76891
47hm4n3 <pixel@canalip293.canalip.upmc.fr>
2018-01-18 12:45:23 +0100

    Added README.md file and lost it's content by a crash of emacs

--- app/src/main/java/com/pixel/kissmyplace/StreetActivity.java
+++ app/src/main/java/com/pixel/kissmyplace/StreetActivity.java
@@ -2,6 +2,7 @@ package com.pixel.kissmyplace;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,8 +15,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Created by pixel on 17/01/2018.
  */
 
-
-public class StreetActivity extends Fragment implements OnMapReadyCallback {
+public class StreetActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -25,7 +25,7 @@ public class StreetActivity extends Fragment implements OnMapReadyCallback {
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.street);
         mapFragment.getMapAsync(this);
     }
 
